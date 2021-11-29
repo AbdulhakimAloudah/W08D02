694a24dc782155abc104e879941db466bc3f68ea
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-29 22:35:22 +0300

    userSchema

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
-const user = new mongoose.Schema({
-  userName: { type: String, required: true },
+const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
- 
+  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
 });
 
-
-module.exports = mongoose.model("User", user);
+module.exports = mongoose.model("User", userSchema);
